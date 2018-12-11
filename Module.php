438c9e5b854e781aece39017f3aab9cdd40f1456
nikitich <nikita.m@powercode.us>
2018-12-11 14:25:23 +0200

    initial

---
+++ Module.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dev-15
+ * Date: 11.12.18
+ * Time: 14:19
+ */
+
+namespace nikitich\simpletranslatemanager;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'nikitich\simpletranslatemanager\controllers';
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Yii2 - Simple Translate Manager
